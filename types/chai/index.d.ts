35c61dec994be43d3501715e57e6bc00394b22d5
Patrick Curry <pwfcurry@users.noreply.github.com>
2019-05-30 16:10:05 +0100

    Added missing deep.contain
    e.g., expect(...).to.deep.contain(...)

--- types/chai/index.d.ts
+++ types/chai/index.d.ts
@@ -183,6 +183,7 @@ declare namespace Chai {
         equals: Equal;
         eq: Equal;
         include: Include;
+        contain: Include;
         property: Property;
         members: Members;
         ordered: Ordered;
